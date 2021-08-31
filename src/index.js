002577cef849a11eaf6eac327ee704e75ac6db27
Miguel Jimenez <migueljimenezaquino@gmail.com>
2021-08-31 18:12:35 -0400

    [ADD]

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 const express = require('express')
+const mongoose = require('mongoose');
+const MONGO_INITDB_ROOT_PASSWORD =process.env.MONGO_INITDB_ROOT_PASSWORD
+const MONGO_INITDB_ROOT_USERNAME =process.env.MONGO_INITDB_ROOT_USERNAME
 const app = express()
- 
+ const port = process.env.WEBSITE_PORT || 8000
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
  
-app.listen(3000)
+app.listen(port)
+const mongoDB= `mongodb://${MONGO_INITDB_ROOT_USERNAME}:${MONGO_INITDB_ROOT_PASSWORD}@0.0.0.0:27017/db_tests`
+console.log('connet mongo db',mongoDB)
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+console.log(`Run import ${port}`)
