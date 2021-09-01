fd0dd555a2327ea90aaddc07171b681baefa2e43
Miguel Jimenez <migueljimenezaquino@gmail.com>
2021-09-01 16:45:26 -0400

    [IMP] add sendgrid

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-const express = require('express')
-const mongoose = require('mongoose');
-const MONGO_INITDB_ROOT_PASSWORD =process.env.MONGO_INITDB_ROOT_PASSWORD
-const MONGO_INITDB_ROOT_USERNAME =process.env.MONGO_INITDB_ROOT_USERNAME
-const app = express()
- const port = process.env.WEBSITE_PORT || 8000
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
- 
-app.listen(port)
-const mongoDB= `mongodb://${MONGO_INITDB_ROOT_USERNAME}:${MONGO_INITDB_ROOT_PASSWORD}@0.0.0.0:27017/db_tests`
-console.log('connet mongo db',mongoDB)
-mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
-console.log(`Run import ${port}`)
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey('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')
+const msg = {
+  to: 'migueljimenezaquino@gmail.com', // Change to your recipient
+  from: 'test@example.com', // Change to your verified sender
+  subject: 'Sending with SendGrid is Fun',
+  text: 'Enviado desde sendgrid',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+}
+sgMail
+  .send(msg)
+  .then(() => {
+    console.log('Email sent')
+  })
+  .catch((error) => {
+    console.error(error)
+  })
